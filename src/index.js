22f584e91b1d119ffc823b8a7a7d6d96afa8af6a
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-02-27 21:06:47 +0530

    changes in config  and envsample

--- src/index.js
+++ src/index.js
@@ -1,7 +1,77 @@
-import mongoose from "mongoose";
+// require('dotenv').config({path:'./env'});
 
-/*
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+
+dotenv.config({
+    path: './env'
+})
+
+
+connectDB()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/* import express from "express";
+   const app = express()
+
  
+Concept of IIFE immediate function exceution, ()()
  
+
+
+// TO have the clean code get semi colon before it start
+
+;(async()=>{
+    try {
+       await mongoose.connect(`${process.env.MONGODB_URI}/${DB_NAME}`);
+       app.on("error",(error)=>{
+        console.log("ERROR: ",error);
+        throw error
+       })
+       app.listen(process.env.PORT,()=>{
+        console.log(`App is listening on port ${process.env.PORT}`)
+       })
+    } catch (error) {
+        console.error("ERROR: ", error);
+        throw error
+        
+    }
+})()
+
 */
-
