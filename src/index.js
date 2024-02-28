b3370a50f39ea497d18c335734291e4838d10235
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-02-28 11:06:19 +0530

    changes in the files

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 // require('dotenv').config({path:'./env'});
 
 import dotenv from "dotenv";
+import { app } from "./app.js";
 import connectDB from "./db/index.js";
 
 
@@ -9,7 +10,18 @@ dotenv.config({
 })
 
 
+
+
 connectDB()
+.then(() =>{
+    app.on
+    app.listen(process.env.PORT || 8000, ()=> {
+        console.log(`Server is running at port : ${process.env.PORT}`)
+    })
+})
+.catch((error) => {
+    console.log("MONGO db connection failed !!!", error)
+})
 
 
 
