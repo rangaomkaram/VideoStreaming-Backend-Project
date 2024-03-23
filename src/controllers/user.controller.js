849599c8470ea04469ae6d5bca21539a6611b4a8
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-03-23 14:37:51 +0530

    where to select the username or email for the user to get login

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -118,7 +118,7 @@ const loginUser = asyncHandler(async(res,req) => {
 
     const {email, username, password} = req.body;
 
-    if(!username || !email){
+    if(!(username || email)){
         throw new ApiError(400, "username or email is required")
     }
 
