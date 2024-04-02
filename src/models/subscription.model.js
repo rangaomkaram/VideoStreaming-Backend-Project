6835c3572e93fb892dec5812948727b479dcabd0
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-04-02 11:16:56 +0530

    added subscription model

--- src/models/subscription.model.js
+++ src/models/subscription.model.js
@@ -3,9 +3,18 @@ import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
 
 const subscriptionSchema = new Schema(
     {
-        
+        subscriber : {
+            type: Schema.Types.ObjectId,
+            ref : "User"
+        },
+        channel : {
+            type: Schema.Types.ObjectId,
+            ref : "User"
+        }
 
-    }
-)
+    }, {timestamps : true}
+);
+
+
 
 export const Subscription = mongoose.model("Subscription", subscriptionSchema)
