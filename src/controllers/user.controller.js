505ff275db69770163b1fca2aed97d7e3cf58588
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-04-08 19:51:20 +0530

    user controller updates

--- src/controllers/user.controller.js
+++ src/controllers/user.controller.js
@@ -453,10 +453,38 @@ const getUserChannelProfile = asyncHandler(async(req, res) => {
                     },
                     channelsSubscribedToCount : {
                         $size: "$subscriberToChannel"
+                    },
+                    isSubscribed:{
+                        $cond: {
+                            if: {$in:[req.user?._id, "$subscribers.subscriber"]},
+                            then : true,
+                            else : false
+                        }
                     }
+                }
+            },
+            {
+                $project: {
+                    fullName: 1,
+                    subscribersCount: 1,
+                    channelsSubscribedToCount : 1,
+                    isSubscribed : 1,
+                    avatar: 1,
+                    coverImage: 1,
+                    email: 1
                 }
             }
         ])
+
+        if(!channel?.length){
+            throw new ApiError(404, "Channel does not exist")
+        }
+
+        return res.status(200)
+        .json ( new ApiResponse(
+                         200, 
+                         channel[0],
+                         "User channel retrived Successfully!"))
 
         
     } catch (error) {
