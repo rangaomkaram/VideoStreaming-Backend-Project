7fe7ecbadbc8cb3d7cdc2195599f927fba35a5ba
omkaram rangasesha uday kumar raju <omkaram.ranga@gmail.com>
2024-04-08 20:06:11 +0530

    removed new keyword at mongoose model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -84,4 +84,4 @@ userSchema.methods.generateRefreshToken = function() {
     )
 }
 
-export const User = new mongoose.model('User', userSchema);
+export const User = mongoose.model('User', userSchema);
